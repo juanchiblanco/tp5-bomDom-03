79a4cf8be598e6e4a74dcffd8e24229658ed4a02
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-05-16 18:56:13 -0300

    cree la funcion agregartarea y agregue dentro los li de las tareas ingresadas y sus respectivos botones de eliminar con clases. Los li ingresan dentro de un ol en html

--- js/app.js
+++ js/app.js
@@ -2,14 +2,35 @@ const tareas = [];
 
 const inputTarea = document.getElementById("inputTarea");
 const btnAgregarTarea = document.querySelector(".btn-primary");
+const listaTareas = document.getElementById("listaTareas");
+
+const agregarTarea = () => {
+  const li = document.createElement("li");
+  li.textContent = inputTarea.value;
+
+  listaTareas.appendChild(li);
+
+  const btnEliminarTarea = document.createElement("button");
+  btnEliminarTarea.textContent = "Eliminar";
+  btnEliminarTarea.classList.add("btn");
+  btnEliminarTarea.classList.add("btn-danger");
+  btnEliminarTarea.classList.add("ms-3");
+
+  li.appendChild(btnEliminarTarea);
+
+  btnEliminarTarea.addEventListener("click", () => {
+    listaTareas.removeChild(li);
+  });
+};
 
 btnAgregarTarea.addEventListener("click", () => {
   const texto = inputTarea.value;
 
   if (texto !== "") {
     tareas.push(texto);
+    agregarTarea();
     inputTarea.value = "";
+  } else {
+    alert("Ingrese una tarea");
   }
 });
-
-console.log(tareas)
